09ef9e3b60db4cf065eda7408df4014312b459b6
Milan Adamovsky <milan.adamovsky+git@gmail.com>
2015-03-26 22:30:35 -0400

    added fill to object

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ literal(x).check("three") // false because x.three does not exist
 literal(x).check(["one", "three"]); // { one : true, three: false }
 literal(x).check(["one", "three"]).any // true because at least x.one exists
 literal(x).check(["one", "three"]).all // false because x.theee does not exist
+literal(x).fill("one", "hello"); // sets "hello" to x.one if it exists, and creates it if doesn't
+literal(x).fill(["one", "two], "hello"); // sets "hello" to x.one and x.two if they exist, and creates it they don't
 ```
 
 # undefined
--- literal.js
+++ literal.js
@@ -40,7 +40,7 @@ function objectTools() {
         path = paths[counter];
 
         if (!nodes[path]) {
-          nodes[path] = loopUntilDoesntExist(path);
+          nodes[path] = exists(path);
           all *= nodes[path];
           any += nodes[path] * 1;
         }
@@ -51,32 +51,87 @@ function objectTools() {
       results.any = !!any;
 
       return results;
-    } else if (pathsType === "string") {
-      return loopUntilDoesntExist(paths);
+    }
+
+    if (pathsType === "string") {
+      return exists(paths);
+    }
+
+    return false;
+  }
+
+  function fill(paths, value) {
+
+    if (!paths) return false;
+
+    var pathsType = findType(paths);
+
+    if (pathsType === "string") {
+
+      if (paths.indexOf(".") < 0) {
+        if (!originalLiteral.hasOwnProperty(paths))
+          originalLiteral[path] = value;
+
+        return originalLiteral[paths];
+      }
+
+      return exists(paths, true, value);
+    }
+
+    if (pathsType === "object") {}
+
+    if (pathsType === "array") {
+      var path;
+      var count = paths.length;
+      var counter = 0;
+      var results = {
+        nodes: {}
+      };
+      var nodes = results.nodes;
+
+      for (; counter < count; counter++) {
+
+        path = paths[counter];
+
+        if (!nodes[path]) {
+          nodes[path] = exists(path, true, value);
+        }
+
+      }
+
+      return results;
     }
 
   }
 
-  function loopUntilDoesntExist(path) {
+  function exists(path, build, value) {
 
     if (path.indexOf(".") < 0)
       return originalLiteral.hasOwnProperty(path);
 
-    var item;
+    var temporaryLiteral = originalLiteral;
     var nodes = path.split(".");
     var count = nodes.length;
     var counter = 0;
+    var node;
 
     for (; counter < count; counter++) {
-      if (!originalLiteral.hasOwnProperty(nodes[item]))
-        return false;
+      node = nodes[counter];
+      if (!temporaryLiteral.hasOwnProperty(node))
+        if (build)
+          temporaryLiteral[node] = counter === count - 1 ? value : {};
+        else
+          return false;
+
+      temporaryLiteral = temporaryLiteral[node];
     }
 
-    return true;
+    return temporaryLiteral;
   }
 
   return {
-    check: check
+    check: check,
+    fill: fill
   };
 }
 
