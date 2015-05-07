85fdaafaa134843bf0be73ed7b33695f39e350b2
Milan Adamovsky <milan.adamovsky+git@gmail.com>
2015-05-06 22:52:14 -0400

    wip

--- modules/undefined.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = function undefinedTools(originalLiteral, originalLiteralType) {
-    return {
-        check: function() {
-            return false;
-        }
-    };
-};
